964a8118a1ddfbe6a534d2db8816682ae2b756a9
Pothuraj <you@example.com>
2024-03-20 22:25:22 -0400

    for Else loop with break, continue

--- Forloop.py
+++ Forloop.py
@@ -18,17 +18,29 @@
 
 
 #Iterating over dictionaries
-friend_ages = { "Rolf" : 25, "Charlie" : 27, "Bob" : 41}    
-for friend in friend_ages:
-    print(friend)   #prints the keys of the dictionaries
+# friend_ages = { "Rolf" : 25, "Charlie" : 27, "Bob" : 41}    
+# for friend in friend_ages:
+#     print(friend)   #prints the keys of the dictionaries
 
-#Reading the values of the dictionaries
-for age in friend_ages.values():
-    print(age)
+# #Reading the values of the dictionaries
+# for age in friend_ages.values():
+#     print(age)
 
-#Reading both the keys and values
-for name,age in friend_ages.items():
-    print(f"Name is {name} and age is {age}")    
+# #Reading both the keys and values
+# for name,age in friend_ages.items():
+#     print(f"Name is {name} and age is {age}")    
+
+
+
+#break and continue
+cars = ["ok","ok","ok","ok","ok","ok","ok"]
+for status in cars:
+    if status == "faulty":
+        break
+    print(f"This car is {status}")
+    print("Shipping new car to customer! ")
+else:
+    print("All cars built successfully . No faulty cars!")
 
 
 
