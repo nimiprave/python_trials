216a6098c1f5307cbf8337c0ab52c4fceda77ec4
Pothuraj <you@example.com>
2024-03-20 21:57:17 -0400

    For loops

---
+++ Forloop.py
@@ -0,0 +1,34 @@
+# friends = ["Rolf", "Bob", "Anne"]
+# for friend in friends:
+#     print(friend)
+
+# #using ranges   
+# for index in range(10):
+#     print(index) 
+
+# #using ranges options
+# for index in range(2,30,5):
+#     print(index) 
+        
+
+#destrucing --example
+# students = [ ("Nimi",30),("Kimi",31),("Dimi",32)]        
+# for name, age in students:
+#     print(f"Name is {name} and age is {age}")
+
+
+#Iterating over dictionaries
+friend_ages = { "Rolf" : 25, "Charlie" : 27, "Bob" : 41}    
+for friend in friend_ages:
+    print(friend)   #prints the keys of the dictionaries
+
+#Reading the values of the dictionaries
+for age in friend_ages.values():
+    print(age)
+
+#Reading both the keys and values
+for name,age in friend_ages.items():
+    print(f"Name is {name} and age is {age}")    
+
+
+
