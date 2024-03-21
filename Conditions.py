f2e302b4519e11d53d98790e289d55337b8e82b1
Pothuraj <you@example.com>
2024-03-20 21:18:11 -0400

    If Statements

---
+++ Conditions.py
@@ -0,0 +1,27 @@
+# friend = "Rolf"
+# user_name = input("Enter your name;")
+
+# if user_name == friend:
+#      print("Hello World")
+#      print("This runs too")
+# else:
+#      print("In Else block")
+
+# print("This is outside of if statement")    
+
+# if 5:
+#      print("Using bool")
+
+
+#Another example
+friends = ["Rolf", "Bob", "Anne"]
+family = ["Jen","Charlie"]
+
+user_name = input( "Entery your name: ")
+
+if user_name in family:
+    print(f"Hello Family! {user_name}")
+elif user_name in friends:
+    print(f"Hello Friend - {user_name}")
+else:
+    print("I don't know you")
