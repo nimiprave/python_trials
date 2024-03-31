53870a09f6f01a3a4a910244f820f73de8281b71
Pothuraj <you@example.com>
2024-03-31 15:22:59 -0400

    Dates example

--- UsingModule.py
+++ UsingModule.py
@@ -1,12 +1,21 @@
 import mymodule as mx  #mx is an alias for the imported module
 import platform
 from mymodule import person1
+import datetime as dt
 
-mx.greetings("Nirmal")
-print(mx.person1)
+# mx.greetings("Nirmal")
+# print(mx.person1)
 
 
-print(platform.system())
-print(platform.machine())
-print(dir(platform))
-print(person1)
+# print(platform.system())
+# print(platform.machine())
+# print(dir(platform))
+# print(person1)
+
+
+#date time operations
+print(dt.datetime.now())
+
+x = dt.datetime(2022,1,1)
+print(dt.datetime(2022,1,1))
+print(x.strftime("%A"))
