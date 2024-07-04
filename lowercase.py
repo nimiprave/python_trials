d19ac33c511b8db4a1be4b081ed983550a5f1425
Pothuraj <you@example.com>
2024-07-04 07:30:25 -0400

    more programs

---
+++ lowercase.py
@@ -0,0 +1,12 @@
+# function to convert the string to lowercase, using the lower level algorithm. (i.e.) ascii table
+# ascii table convertion.
+
+
+print(ord("a"))
+print(ord("A"))
+
+# using the print f function
+
+print(f"The ascii value of A is {ord('A')}")
+
+# ord() and chr() functions can be used to convert the char to ascii and vice versa
---
+++ reading.py
@@ -0,0 +1,10 @@
+name = input("What is your name?   ")
+
+
+# conditions
+if name == "vaishvik":
+    print("Hello vaishvik")
+elif name == "nirmal":
+    print("hello Nirmal")
+else:
+    print("Sorry I don't recognize you")
