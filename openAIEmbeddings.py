3a16409e9ea3f52f9998f9455643c6aca5b696c5
I820262 <nirmal.pothuraj@sap.com>
2024-12-26 10:02:44 -0500

    push the openAiEmbeddings

--- openAIEmbeddings.py
+++ openAIEmbeddings.py
@@ -1,10 +1,11 @@
 from openai import OpenAI
-from transform
+import os
+from dotenv import load_dotenv
 
-apiKey = "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"
+load_dotenv()
+apiKey = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=apiKey)
 response = client.embeddings.create(
     input="The food was delicious and the waiter...", model="text-embedding-ada-002"
 )
 print(response)
-
